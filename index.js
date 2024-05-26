1c3cd66f1e0972341b0567318fe0535b5b5eb03a
lillykml <64362067+lillykml@users.noreply.github.com>
2024-05-26 11:45:20 +0200

    Implemented receiving 1 resource

--- index.js
+++ index.js
@@ -36,6 +36,15 @@ app.get("/api/info", (req, res) => {
     <p>${requestTime}</p>`)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = phonebook.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else res.status(404).end()
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
