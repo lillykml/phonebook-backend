4aca0cde81501d80eacf7f500f7a92f17d9b2f06
lillykml <64362067+lillykml@users.noreply.github.com>
2024-05-27 16:02:35 +0200

    Prepared for deployment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -92,7 +95,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
